bdfcabfdc093b3319c4dd380d4a59e9871353b59
夏天 <47944478+SummerSec@users.noreply.github.com>
2021-02-10 18:38:40 +0800

    Update 1.py

--- 1.py
+++ 1.py
@@ -5,6 +5,11 @@ import time
 import hashlib
 import random
 import string
+import os
+import sys
+import json
+import requests
+import logging
 
 headers = {
     'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/79.0.3945.88 Safari/537.36'
